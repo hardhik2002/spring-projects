781fd6a24f85f880d09110280c6a4f2efc9d9c5c
Hardhik <hardhikreddy1@gmail.com>
2024-11-26 15:17:01 +0530

    restful

---
+++ restful-web-service/src/main/java/com/javatpoint/server/main/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.javatpoint.server.main.controller;
+import org.springframework.web.bind.annotation.GetMapping;  
+import org.springframework.web.bind.annotation.RestController; 
+@RestController
+public class HelloWorldController {
+	@GetMapping(path="/hello-world")
+	public String helloWorld() {
+		return "Hello world";
+	}
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean()
+	{
+		return new HelloWorldBean("Hello World");
+	}
+}
